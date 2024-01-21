e77e08a846a7999e6de8a8f7d971d0ffcfab9c1e
Michał Śledź <michalsledz34@gmail.com>
2024-01-21 19:47:26 +0100

    Initial commit

---
+++ lib/ex_webrtc_dashboard.ex
@@ -0,0 +1,18 @@
+defmodule ExWebrtcDashboard do
+  @moduledoc """
+  Documentation for `ExWebrtcDashboard`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExWebrtcDashboard.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_webrtc_dashboard_test.exs
@@ -0,0 +1,8 @@
+defmodule ExWebrtcDashboardTest do
+  use ExUnit.Case
+  doctest ExWebrtcDashboard
+
+  test "greets the world" do
+    assert ExWebrtcDashboard.hello() == :world
+  end
+end
