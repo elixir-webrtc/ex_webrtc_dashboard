966503afea4635e56f610d60d18183404c1a2e55
Michał Śledź <michalsledz34@gmail.com>
2024-12-02 17:31:27 +0100

    Release v0.7.0

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 ```elixir
 def deps do
   [
-    {:ex_webrtc_dashboard, "~> 0.6.0"}
+    {:ex_webrtc_dashboard, "~> 0.7.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExWebRTCDashboard.MixProject do
   use Mix.Project
 
-  @version "0.6.0"
+  @version "0.7.0"
   @source_url "https://github.com/elixir-webrtc/ex_webrtc_dashboard"
 
   def project do
@@ -42,7 +42,7 @@ defmodule ExWebRTCDashboard.MixProject do
   defp deps do
     [
       {:phoenix_live_dashboard, "~> 0.8.3"},
-      {:ex_webrtc, "~> 0.6.0"},
+      {:ex_webrtc, "~> 0.7.0"},
       {:ex_doc, "~> 0.31.0", only: :dev, runtime: false}
     ]
   end
